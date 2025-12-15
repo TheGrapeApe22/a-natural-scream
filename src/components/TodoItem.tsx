7174034fe0768e5defe98e0a9b2d81c5984ce4bc
Rohan Nadkarni <rnadkarni221@student.fuhsd.org>
2025-12-14 21:29:45 -0800

    make starred an attribute of TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -6,30 +6,32 @@ import XIcon from "../assets/x-icon.png";
 export type Todo = {
 	id: number;
 	text: string;
+	starred: boolean;
 };
 
 type TodoItemProps = {
 	todo: Todo;
 	onChange: (id: number, newText: string) => void;
+	onDelete: (id: number) => void;
+	onToggleStar: (id: number) => void;
 };
 
-export default function TodoItem({ todo, onChange }: TodoItemProps) {
-	const [starred, setStarred] = useState(false);
+export default function TodoItem({ todo, onChange, onDelete, onToggleStar }: TodoItemProps) {
 	return (
 		<div className="todo-item">
 			<button
 				className="icon-button"
-				onClick={() => setStarred((s) => !s)}
+				onClick={() => onToggleStar(todo.id)}
 			>
-				<img src={starred ? StarFilled : StarEmpty} alt={starred ? "★" : "☆"} width="36px"/>
+				<img src={todo.starred ? StarFilled : StarEmpty} alt={todo.starred ? "★" : "☆"} width="36px"/>
 			</button>
 			<input
 				value={todo.text}
 				onChange={(e) => onChange(todo.id, e.target.value)}
 			/>
-			<button className="icon-button">
-                <img src={XIcon} alt="✕" width="36px" />
-            </button>
+			<button className="icon-button" onClick={() => onDelete(todo.id)}>
+				<img src={XIcon} alt="✕" width="36px" />
+			</button>
 		</div>
 	);
 }
--- src/components/TodoPage.tsx
+++ src/components/TodoPage.tsx
@@ -5,25 +5,32 @@ import PlusIcon from "../assets/plus-icon.png";
 
 export default function TodoPage() {
     const [todos, setTodos] = useState<Todo[]>([
-        { id: 1, text: "Buy milk" },
-        { id: 2, text: "Read a book" },
+        { id: 1, text: "Do stuff", starred: false },
     ]);
 
-    const nextId = useRef(3);
+    const nextId = useRef(2);
 
     const handleChange = (id: number, newText: string) => {
         setTodos((prev) => prev.map((t) => (t.id === id ? { ...t, text: newText } : t)));
     };
 
+    const handleDelete = (id: number) => {
+        setTodos((prev) => prev.filter((t) => t.id !== id));
+    };
+
+    const handleToggleStar = (id: number) => {
+        setTodos((prev) => prev.map((t) => (t.id === id ? { ...t, starred: !t.starred } : t)));
+    };
+
     const addTodo = () => {
-        setTodos((prev) => [...prev, { id: nextId.current++, text: "" }]);
+        setTodos((prev) => [...prev, { id: nextId.current++, text: "", starred: false }]);
     };
 
     return (
         <div className="todo-page">
             <div className="todo-list">
                 {todos.map((t) => (
-                    <TodoItem key={t.id} todo={t} onChange={handleChange} />
+                    <TodoItem key={t.id} todo={t} onChange={handleChange} onDelete={handleDelete} onToggleStar={handleToggleStar} />
                 ))}
 
                 <div className="todo-item">
