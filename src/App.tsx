c098552587d8c2e984f04f008ec5f21a59c6f19f
thegrapeape22 <rohan.nadkarni123@gmail.com>
2025-12-30 16:26:40 -0800

    change version number

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,7 @@ function App() {
           className="startup-link"
           onClick={() => startupState === 'hidden' && setStartupState('entering')}
         >
-          A Natural Scream v1.2
+          A Natural Scream v1.3
         </button>
         <div className={`page ${activeTab === 'todo' ? 'visible' : 'hidden'}`} >
           <div className="title">Todo Lists</div>
